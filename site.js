3968af565b4797fd93e9c1a4af71cc67e59e595d
yohgern <125015187+yohgern@users.noreply.github.com>
2023-02-10 17:11:52 +0800

    Add files via upload

---
+++ site.js
@@ -0,0 +1,21 @@
+const ctaButton = document.querySelector('.hero__cta');
+const dtaButton = document.querySelector('.hero__dta');
+const etaButton = document.querySelector('.feature__eta');
+const ftaButton = document.querySelector('.feature__fta');
+const gtaButton = document.querySelector('.feature__gta');
+
+ctaButton.addEventListener('click', () => {
+  alert('Button was clicked!')
+});
+dtaButton.addEventListener('click', () => {
+    alert('Bitch!')
+});
+etaButton.addEventListener('click', () => {
+  alert('HI')
+});
+ftaButton.addEventListener('click', () => {
+  alert('HI')
+});
+gtaButton.addEventListener('click', () => {
+  alert('HI')
+});
